96882daafa3a2212fde9f4380203103a40418616
Josh Cole <saikojosh@gmail.com>
2017-02-17 12:18:54 +0000

    Can't use backticks for property names that aren't computed

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /*
- * ESLint config used by Settled.co.uk. Based on AirBnB`s implementation.
+ * ESLint config used by Settled.co.uk. Based on AirBnB's implementation.
  */
 
 module.exports = {
 
 	extends: [
-		`eslint:recommended`,
-		`plugin:node/recommended`,
+		'eslint:recommended',
+		'plugin:node/recommended',
 	],
 
   parserOptions: {
     ecmaVersion: 6,
-    sourceType: `script`,
+    sourceType: 'script',
     ecmaFeatures: {
       impliedStrict: false,
       jsx: true,
@@ -30,10 +30,10 @@ module.exports = {
   },
 
 	plugins: [
-		`json`,
-    `filenames`,
-		`node`,
-		`promise`,
+		'json',
+    'filenames',
+		'node',
+		'promise',
   ],
 
   rules: {
@@ -41,150 +41,150 @@ module.exports = {
 		/*
 		 * ESLint Core Rules
 		 */
-		`no-await-in-loop`: 2,
-		`no-useless-concat`: 2,
-		`prefer-template`: 2,
-		`quotes`: [2, `backtick`, {
-			`avoidEscape`: true,
+		'no-await-in-loop': 2,
+		'no-useless-concat': 2,
+		'prefer-template': 2,
+		'quotes': [2, 'backtick', {
+			'avoidEscape': true,
 		}],
-    `arrow-body-style`: [1, `as-needed`],
-    `arrow-parens`: 0,
-    `brace-style`: [2, `stroustrup`, {
-      `allowSingleLine`: true
+    'arrow-body-style': [1, 'as-needed'],
+    'arrow-parens': 0,
+    'brace-style': [2, 'stroustrup', {
+      'allowSingleLine': true
     }],
-    `callback-return`: [2, [`callback`, `cb`, `finish`, `done`, `next`, `nextItem`]],
-    `camelcase`: 1,
-    `comma-dangle`: [`error`, {
-      `arrays`: `always-multiline`,
-      `objects`: `always-multiline`,
-      `imports`: `always-multiline`,
-      `exports`: `always-multiline`,
-      `functions`: `never`,
+    'callback-return': [2, ['callback', 'cb', 'finish', 'done', 'next', 'nextItem']],
+    'camelcase': 1,
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'never',
     }],
-    `consistent-return`: 0,
-    `curly`: [2, `all`],
-    `func-names`: 0,
-    `global-require`: 1,
-    `guard-for-in`: 2,
-    `handle-callback-err`: [2, `^err`],
-    `id-length`: [1, {
-      `min`: 3,
-      `max`: 35,
+    'consistent-return': 0,
+    'curly': [2, 'all'],
+    'func-names': 0,
+    'global-require': 1,
+    'guard-for-in': 2,
+    'handle-callback-err': [2, '^err'],
+    'id-length': [1, {
+      'min': 3,
+      'max': 35,
     }],
-    `indent`: [`error`, `tab`, {
-      `SwitchCase`: 1,
-      `VariableDeclarator`: {
-        `var`: 2,
-        `let`: 2,
-        `const`: 3,
+    'indent': ['error', 'tab', {
+      'SwitchCase': 1,
+      'VariableDeclarator': {
+        'var': 2,
+        'let': 2,
+        'const': 3,
       },
-      `outerIIFEBody`: 1,
-      `MemberExpression`: 1,
-      `FunctionDeclaration`: {
-        `parameters`: 1,
-        `body`: 1,
+      'outerIIFEBody': 1,
+      'MemberExpression': 1,
+      'FunctionDeclaration': {
+        'parameters': 1,
+        'body': 1,
       },
-      `FunctionExpression`: {
-        `parameters`: 1,
-        `body`: 1,
+      'FunctionExpression': {
+        'parameters': 1,
+        'body': 1,
       },
-      `CallExpression`: {
-        `arguments`: 1,
+      'CallExpression': {
+        'arguments': 1,
       },
     }],
-    `max-depth`: [2, 10],
-    `max-len`: [1, {
-      `code`: 120,
-      `tabWidth`: 2,
-      `ignoreTrailingComments`: true,
+    'max-depth': [2, 10],
+    'max-len': [1, {
+      'code': 120,
+      'tabWidth': 2,
+      'ignoreTrailingComments': true,
     }],
-    `max-nested-callbacks`: [2, 5],
-    `max-params`: [2, 5],
-    `new-cap`: [2, {
-      `newIsCap`: true,
-      `capIsNew`: true,
-      `capIsNewExceptions`: [`Router`],
+    'max-nested-callbacks': [2, 5],
+    'max-params': [2, 5],
+    'new-cap': [2, {
+      'newIsCap': true,
+      'capIsNew': true,
+      'capIsNewExceptions': ['Router'],
     }],
-    `newline-after-var`: [2, `always`],
-    `no-cond-assign`: [2, `except-parens`],
-    `no-confusing-arrow`: 2,
-    `no-console`: 1,
-    `no-continue`: 0,
-    `no-else-return`: 0,
-    `no-lonely-if`: 2,
-    `no-mixed-requires`: [1, {
-      `grouping`: true,
-      `allowCall`: true,
+    'newline-after-var': [2, 'always'],
+    'no-cond-assign': [2, 'except-parens'],
+    'no-confusing-arrow': 2,
+    'no-console': 1,
+    'no-continue': 0,
+    'no-else-return': 0,
+    'no-lonely-if': 2,
+    'no-mixed-requires': [1, {
+      'grouping': true,
+      'allowCall': true,
     }],
-    `no-new-require`: 2,
-    `no-param-reassign`: [2, {
-      `props`: false,
+    'no-new-require': 2,
+    'no-param-reassign': [2, {
+      'props': false,
     }],
-    `no-path-concat`: 2,
-    `no-plusplus`: [1, {
-      `allowForLoopAfterthoughts`: true,
+    'no-path-concat': 2,
+    'no-plusplus': [1, {
+      'allowForLoopAfterthoughts': true,
     }],
-    `no-process-exit`: 2,
-    `no-return-assign`: 0,
-    `no-shadow`: [2, {
-      `allow`: [`err`],
+    'no-process-exit': 2,
+    'no-return-assign': 0,
+    'no-shadow': [2, {
+      'allow': ['err'],
     }],
-    `no-sync`: 2,
-    `no-tabs`: 0,
-    `no-underscore-dangle`: 0,
-    `no-unexpected-multiline`: 2,
-    `no-unused-expressions`: [2, {
-      `allowTernary`: true,
+    'no-sync': 2,
+    'no-tabs': 0,
+    'no-underscore-dangle': 0,
+    'no-unexpected-multiline': 2,
+    'no-unused-expressions': [2, {
+      'allowTernary': true,
     }],
-    `no-unused-vars`: [2, {
-      `vars`: `all`,
-      `args`: `after-used`,
+    'no-unused-vars': [2, {
+      'vars': 'all',
+      'args': 'after-used',
     }],
-    `no-with`: 2,
-    `object-curly-spacing`: [2, `always`],
-    `one-var`: [2, `never`],
-    `padded-blocks`: 0,
-    `prefer-arrow-callback`: 1,
-    `prefer-const`: 2,
-    `prefer-spread`: 2,
-    `radix`: 0,
-    `semi`: [2, `always`],
-    `space-before-blocks`: [1, {
-      `functions`: `always`,
-      `keywords`: `always`,
-      `classes`: `always`,
+    'no-with': 2,
+    'object-curly-spacing': [2, 'always'],
+    'one-var': [2, 'never'],
+    'padded-blocks': 0,
+    'prefer-arrow-callback': 1,
+    'prefer-const': 2,
+    'prefer-spread': 2,
+    'radix': 0,
+    'semi': [2, 'always'],
+    'space-before-blocks': [1, {
+      'functions': 'always',
+      'keywords': 'always',
+      'classes': 'always',
     }],
-    `space-before-function-paren`: [1, {
-      `anonymous`: `always`,
-      `named`: `always`,
+    'space-before-function-paren': [1, {
+      'anonymous': 'always',
+      'named': 'always',
     }],
-    `spaced-comment`: 1,
-    `strict`: [2, `global`],
-    `yoda`: [2, `never`],
+    'spaced-comment': 1,
+    'strict': [2, 'global'],
+    'yoda': [2, 'never'],
 
 		/*
 		 * eslint-plugin-filenames
 		 */
-		 `filenames/match-regex`: 2,  // Filenames must be in `camelCase`.
-		 `filenames/no-index`: 2,
+		 'filenames/match-regex': 2,  // Filenames must be in 'camelCase'.
+		 'filenames/no-index': 2,
 
 		 /*
 		  * eslint-plugin-node
 			*/
-		`node/exports-style`: [2, `module.exports`],
+		'node/exports-style': [2, 'module.exports'],
 
 		/*
 		 * eslint-plugin-promise
 		 */
-		 `promise/always-return`: 2,
-		 `promise/no-return-wrap`: 1,
-		 `promise/param-names`: 2,
-		 `promise/catch-or-return`: 2,
-		 `promise/no-native`: 0,
-		 `promise/no-nesting`: 2,
-		 `promise/no-promise-in-callback`: 1,
-		 `promise/no-callback-in-promise`: 1,
-		 `promise/avoid-new`: 0,
+		 'promise/always-return': 2,
+		 'promise/no-return-wrap': 1,
+		 'promise/param-names': 2,
+		 'promise/catch-or-return': 2,
+		 'promise/no-native': 0,
+		 'promise/no-nesting': 2,
+		 'promise/no-promise-in-callback': 1,
+		 'promise/no-callback-in-promise': 1,
+		 'promise/avoid-new': 0,
 		 'promise/prefer-await-to-then': 0,  // Will be enabled once Node.js 8.0 is released.
 		 'promise/prefer-await-to-callbacks': 0,  // Will be enabled once Node.js 8.0 is released.
 
